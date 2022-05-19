b0bf79d2f7d3c043484a27ab7b8fb99499a30b80
AryanKalaria <66237712+AryanKalaria@users.noreply.github.com>
2022-05-19 06:09:33 +0530

    Update stack.py

--- Balanced Paren/stack.py
+++ Balanced Paren/stack.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 class Stack():
     def __init__(self):
         self.items = []
@@ -22,20 +17,3 @@ class Stack():
     def peek(self):
         if not self.is_empty():
             return self.items[-1]
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    s = Stack()
-    print(s.is_empty())
-    s.push("A")
-    s.push("B")
-    s.push("C")
-    s.push("D")
-    print(s.get_stack())
-    print(s.pop())
-    print(s.pop())
-    print(s.get_stack())
-    print(s.peek())
-    print(s.is_empty())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
